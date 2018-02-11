05e5469a2c6c809a162030271680d574f33e4c90
John Deady <35745286+JohnADeady@users.noreply.github.com>
2018-02-11 22:33:50 +0000

    Create Collatz.py

---
+++ Collatz.py
@@ -0,0 +1,13 @@
+# John Deady, 07.02.2018
+# The Collatz Conjecture
+
+var = input("Enter something:")
+
+while x > 1:    
+
+ if x % 2 == 0:
+    x = x // 2  
+    print(x)
+ else:
+    x = (3 * x) + 1   
+    print(x)
