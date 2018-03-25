14022346445fb253fd80fcf59454762a08b19a0c
John Deady <35745286+JohnADeady@users.noreply.github.com>
2018-03-25 12:24:04 +0100

    Update Madl.py
    I updated the instructions, comments and references.

--- Madl.py
+++ Madl.py
@@ -2,12 +2,16 @@
 # Mad Libs Generator
 
 # Adapted  from https://knightlab.northwestern.edu/2014/06/05/five-mini-programming-projects-for-the-python-beginner/
+# The program will first prompt the user for a series of inputs a la Mad Libs.
+#Then, once all the information has been inputted, the program will take that data and place them into a premade story template. 
+# You’ll need prompts for user input, and to then print out the full story at the end with the input included.
+# To find a solution to this problem I used Stackflow.
 
-print ("welcome to Mad Libs")
+Print ("welcome to Mad Libs")                           # Inspired by Summer Son’s Mad Libs project with Javascript
 
-adjective = input("Please input an adjective:")
-noun = input ("Please input a noun:")
-animal = input("Please input an animal:")
-noise = input ("Please input a noise:")
+adjective = input("Please input an adjective:")         # You will be prompted into an adjective
+noun = input ("Please input a noun:")                   # You will be prompted into an noun
+animal = input("Please input an animal:")               # You will be prompted into an animal
+noise = input ("Please input a noise:")                 # You will be prompted into an noise
 
-print(adjective, "Macdougal", "had", "a", noun, noise)
+print(adjective, "Macdougal", "had", "a", noun, noise)  # This will print out a line with your inputs
