4085a733287077b5d2184d55baf0422c760a9006
John Deady <35745286+JohnADeady@users.noreply.github.com>
2018-03-26 21:32:49 +0100

    Update Euler1.py
    Added comments to this file

--- Euler1.py
+++ Euler1.py
@@ -6,10 +6,10 @@
 i = 1                                  # let i = 1 as your starting integer
 sum = 0                                # let sum = 0 this is needed in order to sum multiplies
 
-while i < 1000:                        
- if (i % 3 == 0) or (i % 5 == 0):
-    sum = sum + i
- i = i + 1 
+while i < 1000:                        # This is a while loop. We are looking for multiples of 3 and 5 below 1000.
+ if (i % 3 == 0) or (i % 5 == 0):      # if statement. If i is divided by 3 or 5 and there is no remainder
+    sum = sum + i                      # We add all the multiples of 3 and 5 together
+ i = i + 1                             # We will add 1 to i until we get to 1000
 
-print("Sum of the multiplies of 3 and 5:", sum)
+print("Sum of the multiplies of 3 and 5:", sum) # This will print the sum of the multiples of 3 and 5
 
