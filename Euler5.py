551169b8d997a90decaedbb3686324bf4614c0e2
John Deady <35745286+JohnADeady@users.noreply.github.com>
2018-03-28 23:13:04 +0100

    Update Euler5.py
    Added extra comments and updated code

--- Euler5.py
+++ Euler5.py
@@ -6,14 +6,14 @@
 # I have used stackflow in order to solve this problem. 
 # We take prime numbers only. Integers (6,8,9,10,12,15,20) are divisibles by 2,3 and 5.
 # We multiply all the prime numbers together to get 9,699,690
-# Then
+# Then while loops calculates 24 from p.
 
 primes = [2,3,5,7,11,13,17,19]          # Let primes equal to the prime numbers up to 20.
 x = 1                                   # Let x = 1
 for p in primes:                        # for statemtent. P equals to primes.
-    n = 2                               # Let n = 2
     x *= p                              # x is equal to x multiply p (i.e.2*3*5*7*11*17*19)
-    while (p**n < 21):                  # This is a while loop. This while loop states p to the power of n is less than 21.
+    n = 2                               # We let n = 2 
+    while (p**n < 21):                  # This is a while loop. This while loop states while p to the power of n is less than 21.
         x *= p                          # x is equal to x multiply p
         n += 1                          # We continue adding 1 to n until 20.
 
