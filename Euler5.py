c70e305a9a8cc39095adadabae0cc3c6baf79aa2
John Deady <35745286+JohnADeady@users.noreply.github.com>
2018-03-28 23:00:44 +0100

    Updated Euler5.py
    Added references and comments

--- Euler5.py
+++ Euler5.py
@@ -1,19 +1,21 @@
 # John Deady, 2018-23-02
 # Euler Project Smallest Number 
-
+# 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+# What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 # Adapted from https://projecteuler.net/problem=5
+# I have used stackflow in order to solve this problem. 
+# We take prime numbers only. Integers (6,8,9,10,12,15,20) are divisibles by 2,3 and 5.
+# We multiply all the prime numbers together to get 9,699,690
+# Then
 
-# I have a range of 1 to 20. Find the smallest number that 
-# all can evenly be divide into it.
+primes = [2,3,5,7,11,13,17,19]          # Let primes equal to the prime numbers up to 20.
+x = 1                                   # Let x = 1
+for p in primes:                        # for statemtent. P equals to primes.
+    n = 2                               # Let n = 2
+    x *= p                              # x is equal to x multiply p (i.e.2*3*5*7*11*17*19)
+    while (p**n < 21):                  # This is a while loop. This while loop states p to the power of n is less than 21.
+        x *= p                          # x is equal to x multiply p
+        n += 1                          # We continue adding 1 to n until 20.
 
-primes = [2,3,5,7,11,13,17,19]
-x = 1 
-for p in primes:
-    n = 2
-    x *= p
-    while (p**n < 21):
-        x *= p
-        n += 1
-
-print (x)
+print (x)                               # Print x (The smallest positive number)
 
