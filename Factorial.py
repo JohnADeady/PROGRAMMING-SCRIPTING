e608547a86edfff3c7f0799c8fc7604752ffffc4
John Deady <35745286+JohnADeady@users.noreply.github.com>
2018-03-29 20:28:25 +0100

    Update Factorial.py
    Change code to a recursive factorial function

--- Factorial.py
+++ Factorial.py
@@ -6,13 +6,14 @@
 # Find the factorial of 5,7 and 10.
 # I used Khan Academy, Stackflow and Codementor to solve this.
 # We use a defined function so then we can re-use this code for each integer 5,7 & 10. 
+# Recursive Factorial Function - Using the function in the code
 
 def factorial (i):                             # A defined function called factorial for i.         
-    number = 1                                 # Let number equal to 1
-    while i >= 1:                              # This is a while loop. While i is greater than or equal to one we continue so 
-        number *= i                            # We are multiplying all the integers of i by num
-        i -= 1                                 # We are going to take 1 off i every loop
-    return number
+    if n <= 1:                                 # If n is greater or equal to one we will get a result of one
+        return 1                               # Return 1
+    else:                                      # If not greater or equal to one
+        n = n * factorial(n - 1)               # We multiply n by itself and function itself less n minus 1 [ie if i = 2 n = 2 * (2-1)]
+    return n   
     
 print (" the factorial of number 5 is:" , factorial (5))
 print (" the factorial of number 7 is:" , factorial (7))
