ddefc753e966e959ce509b2b0b2d7bb725cab8db
John Deady <35745286+JohnADeady@users.noreply.github.com>
2018-04-07 15:16:19 +0100

    Update Dice.py
    Updated a comment

--- Dice.py
+++ Dice.py
@@ -4,7 +4,6 @@
 # Adapted from https://knightlab.northwestern.edu/2014/06/05/five-mini-programming-projects-for-the-python-beginner/
 # The Goal: this program involves writing a program that simulates a rolling dice. 
 # When the program runs, it will randomly choose a number between 1 and 6.   
-# Or whatever other integer you prefer — the number of sides on the dice is up to you. The program will print what that number is.
 # It should then ask you if you’d like to roll again.
 # For this project, you’ll need to set the min and max number that your dice can produce i.e minimum of 1 and a maximum of 6.
 # You’ll also want a function that randomly grabs a number within that range and prints it.
