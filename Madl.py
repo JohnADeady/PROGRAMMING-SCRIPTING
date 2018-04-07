9b2d2839db76033364da96aaaebf1f31511a5269
John Deady <35745286+JohnADeady@users.noreply.github.com>
2018-04-07 15:30:40 +0100

    Update Madl.py
    Updated comments

--- Madl.py
+++ Madl.py
@@ -3,7 +3,7 @@
 
 # Adapted  from https://knightlab.northwestern.edu/2014/06/05/five-mini-programming-projects-for-the-python-beginner/
 # The program will first prompt the user for a series of inputs a la Mad Libs.
-#Then, once all the information has been inputted, the program will take that data and place them into a premade story template. 
+# Then, once all the information has been inputted, the program will take that data and place them into a premade story template. 
 # You’ll need prompts for user input, and to then print out the full story at the end with the input included.
 # To find a solution to this problem I used Stackflow.
 
