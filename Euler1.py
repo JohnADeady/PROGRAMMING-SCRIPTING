c144ddd38cbc820b1da0752d4721b1947224de3c
John Deady <35745286+JohnADeady@users.noreply.github.com>
2018-04-07 15:18:52 +0100

    Update Euler1.py
    Updated comment

--- Euler1.py
+++ Euler1.py
@@ -7,7 +7,7 @@ i = 1                                  # let i = 1 as your starting integer
 sum = 0                                # let sum = 0 this is needed in order to sum multiplies
 
 while i < 1000:                        # This is a while loop. We are looking for multiples of 3 and 5 below 1000.
- if (i % 3 == 0) or (i % 5 == 0):      # if statement. If i is divided by 3 or 5 and there is no remainder
+ if (i % 3 == 0) or (i % 5 == 0):      # if statement. If i is divided by 3 or 5 and there is no remainder i.e. it's a even integer
     sum = sum + i                      # We add all the multiples of 3 and 5 together
  i = i + 1                             # We will add 1 to i until we get to 1000
 
